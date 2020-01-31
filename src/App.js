caa3d03dcc9112777d2b26190ba5a8f69b567eda
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-31 19:44:56 -0300

    replace to functional components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 
+import WeatherBox from './components/organisms/WeatherBox';
+import LocalizationBox from './components/organisms/LocalizationBox';
+import SearchBox from './components/organisms/SearchBox';
+
+require('dotenv').config();
+
 const api = {
-  key: "78144cbaa0439c70d1ec3f8f95037759",
-  base: "https://api.openweathermap.org/data/2.5/"
+  key: process.env.local ? process.env.local.REACT_APP_KEY : process.env.REACT_APP_KEY,
+  base: 'https://api.openweathermap.org/data/2.5/'
 }
 
 function App() {
@@ -10,60 +16,28 @@ function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
-  const search = evt => {
-    if(evt.key === 'Enter'){
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-        });
-    }
-  }
-  
-  const dateBuilder = d => {
-    let months = ["January","February","March","April","May","June","July"
-                 ,"August","September","October","November","December"];
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day}, ${date} ${month} ${year}`;
-  }
-
   return (
     <div className={(typeof weather.main != "undefined") 
         ? ((weather.main.temp > 16) 
         ? 'app warm' : 'app') 
         : 'app'} >
       <main>
-        <div className="search-box">
-          <input 
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
+        <SearchBox 
+          setQuery={setQuery}
+          query={query}
+          api={api}
+          setWeather={setWeather}
+        />
         {(typeof weather.main != "undefined") ? (
           <>
-            <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
-                <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="weather-box">
-              <div className="temp">
-                {Math.round(weather.main.temp)}°c
-              </div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
+            <LocalizationBox 
+              weather={weather}
+            />
+            <WeatherBox 
+              weather={weather}
+            />
           </>
-          ):(<></>)}
+          ):(<></>)}       
       </main>
     </div>
   );
