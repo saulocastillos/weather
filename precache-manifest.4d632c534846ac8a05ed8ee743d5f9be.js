1f01e4df784b7ad712f7ad38947e45ed5cf62a3b
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-31 23:22:40 -0300

    Updates

---
+++ precache-manifest.4d632c534846ac8a05ed8ee743d5f9be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad134b9b7fa90fe54ccf24d8ef49e53",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "b04624b1ca932b4540fb",
+    "url": "/weather/static/js/2.41e33c5a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/weather/static/js/2.41e33c5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b744ef1944ffca83ef",
+    "url": "/weather/static/js/main.1a53c8c3.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
